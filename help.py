dad6c1ce522b3fdf7520f53d1996151440c3b013
levon <airtk@ya.ru>
2021-02-10 17:49:34 +0300

    перезапуск когда гол

--- help.py
+++ help.py
@@ -1,5 +1,5 @@
 import pygame
-
+schet_ochkov_poymaneh_kakashek = pygame.font.SysFont("arial", 25, True, False)
 
 def izmeni_kartinku(kartinka, shirina, visota, uberi_cvet, porog):
     kartinka = pygame.transform.scale(kartinka, [shirina, visota])
@@ -11,3 +11,16 @@ def izmeni_kartinku(kartinka, shirina, visota, uberi_cvet, porog):
     m1.to_surface(q2, kartinka, None, None, None)
     # q2.set_colorkey([0, 0, 0])
     return q2
+
+
+
+
+
+def schet_ochkov(screen, color, shet, raund):
+    ert = str(raund)
+    qwer = str(shet)
+    poi = "  РАУНД: " + ert + " " + qwer + " ПОЙМАНЫЕ КАКАШКИ "
+    # schet_ochkov_poymaneh_kakashek_kartinka=schet_ochkov_poymaneh_kakashek.render(qwer,True,color)
+    schet_ochkov_poymaneh_kakashek_kartinka = schet_ochkov_poymaneh_kakashek.render(poi, True, color)
+
+    screen.blit(schet_ochkov_poymaneh_kakashek_kartinka, [0, 0])
